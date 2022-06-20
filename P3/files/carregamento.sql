42fbc35d0c8e855854d44c881749cde12008fa01
Alexisminas <alexisminas308@gmail.com>
2022-06-20 19:51:38 +0100

    organized stuff

---
+++ P3/files/carregamento.sql
@@ -0,0 +1,124 @@
+
+insert into ivm values(54623, 'Mars Inc.');
+insert into ivm values(91824, 'Mars Inc.');
+insert into ivm values(91734, 'Paytec');
+insert into ivm values(84712, 'Seaga');
+
+insert into category values('Águas');
+insert into category values('Vegetais');
+insert into category values('Tubérculos');
+insert into category values('Laticínios');
+insert into category values('Congelados');
+insert into category values('Pão congelado');
+insert into category values('Carne');
+insert into category values('Porco');
+insert into category values('Vinhos');
+insert into category values('Leite');
+insert into category values('Batata');
+insert into category values('Cereais');
+insert into category values('Doces');
+insert into category values('Tabletes');
+
+insert into simple_category values('Tabletes');
+insert into simple_category values('Leite');
+insert into simple_category values('Águas');
+insert into super_category values('Vinhos');
+insert into super_category values('Tubérculos');
+insert into simple_category values('Porco');
+insert into simple_category values('Pão congelado');
+insert into super_category values('Congelados');
+insert into super_category values('Laticínios');
+insert into super_category values('Vegetais');
+insert into simple_category values('Cereais');
+insert into simple_category values('Batata');
+insert into super_category values('Carne');
+insert into super_category values('Doces');
+
+insert into product values('2466881097426', 'Porco','Orelha de porco');
+insert into product values('2916348454168', 'Batata', 'Batata');
+insert into product values('3072558779790', 'Cereais', 'Cereais Chocapic');
+insert into product values('6010440030530', 'Laticínios', 'Iogurte de morango');
+insert into product values('6608024404321', 'Vinhos', 'Vinho Branco');
+insert into product values('7009667363536', 'Vinhos', 'Vinho Tinto');
+insert into product values('7559992648401', 'Vegetais', 'Couve-flor');
+insert into product values('7441503902471', 'Laticínios', 'Leite de chocolate');
+insert into product values('7569290774222', 'Tabletes', 'Chocolate Milka');
+insert into product values('8079544896855', 'Doces', 'Pintarolas');
+insert into product values('8407837792023', 'Vegetais', 'Cenoura');
+insert into product values('8451711541269', 'Carne','Frango inteiro');
+insert into product values('9535718049874', 'Águas', 'Água Luso');
+insert into product values('9852223948688', 'Águas', 'Água Penacova');
+
+---
+
+insert into has_other values('Tubérculos', 'Batata');
+insert into has_other values('Doces', 'Tabletes');
+insert into has_other values('Carne', 'Porco');
+insert into has_other values('Congelados', 'Pão congelado');
+insert into has_other values('Laticínios', 'Leite');
+
+insert into shelf values(1, 54623, 'Mars Inc.', 60.00, 'Tabletes');
+insert into shelf values(2, 54623, 'Mars Inc.', 40.00, 'Tubérculos');
+insert into shelf values(1, 84712, 'Seaga', 100.00, 'Porco');
+insert into shelf values(3, 84712, 'Seaga', 100.00, 'Doces');
+insert into shelf values(4, 84712, 'Seaga', 70.00, 'Vegetais');
+insert into shelf values(1, 91734, 'Paytec', 100.00, 'Leite');
+insert into shelf values(3, 91734, 'Paytec', 60.00, 'Águas');
+insert into shelf values(1, 91824, 'Mars Inc.', 100.00, 'Carne');
+insert into shelf values(2, 91824, 'Mars Inc.', 20.00, 'Doces');
+insert into shelf values(4, 91824, 'Mars Inc.', 30.00, 'Leite');
+
+insert into planogram values('2916348454168', 1, 54623, 'Mars Inc.', 5, 4, 3);
+insert into planogram values('8079544896855', 1, 54623, 'Mars Inc.', 3, 10, 1);
+insert into planogram values('9535718049874', 1, 54623, 'Mars Inc.', 2, 3, 1);
+insert into planogram values('7569290774222', 2, 54623, 'Mars Inc.', 1, 20, 0);
+insert into planogram values('8407837792023', 3, 84712, 'Seaga', 3, 10, 1);
+insert into planogram values('9535718049874', 4, 84712, 'Seaga', 4, 1, 2);
+insert into planogram values('8079544896855', 2, 91824, 'Mars Inc.', 1, 5, 2);
+insert into planogram values('7441503902471', 1, 91734, 'Paytec', 3, 10, 1);
+insert into planogram values('9535718049874', 1, 91824, 'Mars Inc.', 1, 2, 1);
+
+
+insert into point_of_retail values('Galp', 'Lisboa', 'Loures');
+insert into point_of_retail values('Repsol', 'Lisboa', 'Cidade Nova');
+insert into point_of_retail values('Prio', 'Algarve', 'Montegordo');
+insert into point_of_retail values('Diesel', 'Alentejo', 'Porto Covo');
+
+insert into installed_at values(54623, 'Mars Inc.', 'Galp');
+insert into installed_at values(91824, 'Mars Inc.', 'Repsol');
+insert into installed_at values(84712, 'Seaga', 'Diesel');
+insert into installed_at values(91734, 'Paytec', 'Diesel');
+
+insert into retailer values('909025806', 'Solbel');
+insert into retailer values('901013754', 'Continente');
+insert into retailer values('907546188', 'Jerónimo Martins');
+insert into retailer values('902712215', 'E.Leclerc');
+insert into retailer values('901748680', 'Losk');
+insert into retailer values('906996794', 'Niuma');
+
+insert into responsible_for values('54623', 'Mars Inc.', '909025806', 'Doces');
+insert into responsible_for values('54623', 'Mars Inc.', '901013754', 'Tabletes');
+insert into responsible_for values('54623', 'Mars Inc.', '907546188', 'Tubérculos');
+insert into responsible_for values('54623', 'Mars Inc.', '906996794', 'Águas');
+insert into responsible_for values('91824', 'Mars Inc.', '906996794', 'Águas');
+insert into responsible_for values('91824', 'Mars Inc.', '909025806', 'Doces');
+insert into responsible_for values('91824', 'Mars Inc.', '901748680', 'Carne');
+insert into responsible_for values('84712', 'Seaga', '901013754', 'Vegetais');
+insert into responsible_for values('84712', 'Seaga', '902712215', 'Cereais');
+insert into responsible_for values('84712', 'Seaga', '901748680', 'Águas');
+insert into responsible_for values('91734', 'Paytec', '906996794', 'Águas');
+insert into responsible_for values('91734', 'Paytec', '909025806', 'Porco');
+insert into responsible_for values('91734', 'Paytec', '902712215', 'Leite');
+
+insert into replenishment_event values('9535718049874', 1, '54623', 'Mars Inc.', '26/11/2022', 3, '906996794');
+insert into replenishment_event values('9535718049874', 1, '54623', 'Mars Inc.', '25/11/2022', 3, '906996794');
+insert into replenishment_event values('9535718049874', 1, '54623', 'Mars Inc.', '24/11/2022', 3, '906996794');
+insert into replenishment_event values('7569290774222', 2, '54623', 'Mars Inc.', '24/10/2022', 2, '901013754');
+
+insert into replenishment_event values('8079544896855', 2, '91824', 'Mars Inc.', '17/4/2021', 4, '909025806');
+insert into replenishment_event values('9535718049874', 1, '91824', 'Mars Inc.', '6/10/2022', 2, '906996794');
+
+insert into replenishment_event values('8407837792023', 3, '84712', 'Seaga', '4/1/2022', 3, '901013754');
+insert into replenishment_event values('9535718049874', 4, '84712', 'Seaga', '3/1/2022', 2, '901748680');
+
+insert into replenishment_event values('7441503902471', 1, '91734', 'Paytec', '11/10/2021', 1, '902712215');
---
+++ P3/files/save/ivm.sql
@@ -0,0 +1,306 @@
+drop table if exists ivm cascade;
+drop table if exists category cascade;
+drop table if exists simple_category cascade;
+drop table if exists super_category cascade;
+drop table if exists has_other cascade;
+drop table if exists product cascade;
+drop table if exists has_category cascade;
+drop table if exists point_of_retail cascade;
+drop table if exists installed_at cascade;
+drop table if exists shelf cascade;
+drop table if exists planogram cascade;
+drop table if exists retailer cascade;
+drop table if exists responsible_for cascade;
+drop table if exists replenishment_event cascade;
+----------------------------------------
+-- Table Creation
+----------------------------------------
+
+-- Named constraints are global to the database.
+-- Therefore the following use the following naming rules:
+--   1. pk_table for names of primary key constraints
+--   2. fk_table_another for names of foreign key constraints
+
+create table category (
+	category_name varchar(255) not null,
+	constraint pk_category primary key(category_name)
+);
+
+create table simple_category (
+	category_name varchar(255) not null,
+	constraint pk_simple_category primary key(category_name),
+	constraint fk_simple_category_category foreign key(category_name) references category(category_name)
+);
+
+create table super_category (
+	category_name varchar(255) not null,
+	constraint pk_super_category primary key(category_name),
+	constraint fk_super_category_category foreign key(category_name) references category(category_name)
+);
+
+create table has_other (
+	super_category varchar(255) not null,
+	child_category varchar(255) not null,
+	constraint pk_has_other primary key(child_category),
+	constraint fk_has_other_super_category foreign key(super_category) references super_category(category_name),
+	constraint fk_has_other_category foreign key(child_category) references category(category_name)
+);
+
+CREATE OR REPLACE FUNCTION chk_category_loop()
+RETURNS TRIGGER AS
+$$
+BEGIN 
+	IF NEW.super_category = NEW.child_category THEN
+		RAISE EXCEPTION 'A category must not contain itself';
+	END IF;
+
+	RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER chk_category_loop
+BEFORE UPDATE OR INSERT ON has_other
+FOR EACH ROW EXECUTE PROCEDURE chk_category_loop();
+
+create table product (
+	product_ean char(13) not null,
+	category_name varchar(255) not null,
+	product_descr varchar(255),
+	constraint pk_product primary key(product_ean),
+	constraint fk_product_category foreign key(category_name) references category(category_name)
+);
+
+create table has_category (
+	product_ean char(13) not null,
+	category_name varchar(255) not null,
+	constraint pk_has_category primary key(product_ean, category_name),
+	constraint fk_has_category_product foreign key(product_ean) references product(product_ean),
+	constraint fk_has_category_category foreign key(category_name) references category(category_name)
+);
+
+CREATE OR REPLACE FUNCTION chk_insert_product()
+RETURNS TRIGGER AS
+$$
+BEGIN
+	INSERT INTO has_category
+	VALUES (NEW.product_ean, NEW.category_name);
+	RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER chk_insert_product
+AFTER INSERT ON product
+FOR EACH ROW EXECUTE PROCEDURE chk_insert_product();
+
+CREATE OR REPLACE FUNCTION chk_update_product()
+RETURNS TRIGGER AS
+$$
+BEGIN
+	UPDATE has_category
+	SET category_name = NEW.category_name
+	WHERE product_ean = NEW.product_ean;
+	RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER chk_update_product
+AFTER UPDATE ON product
+FOR EACH ROW EXECUTE PROCEDURE chk_update_product();
+
+
+create table ivm (
+	ivm_serial_number numeric(5,0) not null, -- numeric ?
+	ivm_manuf varchar(255) not null, -- varchar ?
+	constraint pk_ivm primary key(ivm_serial_number, ivm_manuf)
+);
+
+create table point_of_retail (
+	point_name varchar(255) not null,
+	point_district varchar(255) not null,
+	point_municipality varchar(255) not null,
+	constraint pk_point_of_retail primary key(point_name)
+);
+
+create table installed_at (
+	ivm_serial_number numeric(5,0) not null,
+	ivm_manuf varchar(255) not null,
+	point_name varchar(255) not null,
+	constraint pk_installed_at primary key(ivm_manuf, ivm_serial_number, point_name),
+	constraint fk_installed_at_ivm foreign key(ivm_manuf, ivm_serial_number) references ivm(ivm_manuf, ivm_serial_number),
+	constraint fk_installed_at_point_of_retail foreign key(point_name) references point_of_retail(point_name)
+);
+
+create table shelf (
+	shelf_nr numeric(16,0) not null, --?
+	ivm_serial_number numeric(5,0) not null,
+	ivm_manuf varchar(255) not null,
+	shelf_height numeric(5, 2) not null, -- ?
+	category_name varchar(255) not null,
+	constraint pk_shelf primary key(shelf_nr, ivm_serial_number, ivm_manuf),
+	constraint fk_shelf_ivm foreign key(ivm_serial_number, ivm_manuf) references ivm(ivm_serial_number, ivm_manuf),
+	constraint fk_shelf_category foreign key(category_name) references category(category_name)
+);
+
+create table planogram (
+	product_ean char(13) not null,
+	shelf_nr numeric(16,0) not null,
+	ivm_serial_number numeric(5,0) not null,
+	ivm_manuf varchar(255) not null,
+	faces_seen numeric(16, 0) not null,
+	units numeric(3, 0) not null,
+	loc numeric(3, 0) not null,
+	constraint pk_planogram primary key(product_ean, shelf_nr, ivm_serial_number, ivm_manuf),
+	constraint fk_planogram_product foreign key(product_ean) references product(product_ean),
+	constraint fk_planogram_shelf foreign key(shelf_nr, ivm_serial_number, ivm_manuf) references shelf(shelf_nr, ivm_serial_number, ivm_manuf)
+);
+
+
+--------
+--- Um Produto só pode ser reposto numa Prateleira que apresente 
+---(pelo menos) uma das Categorias desse produto
+--  Cenas q se sabe:
+--  -> 1 produto, 1 pelo menos 1 categoria
+--  -> 1 prateleira numa dada maquina, 1 categoria
+--  PassosA:
+--  1o, Ver Prateleira
+--  2o, Ver categoria da Prateleira
+-- select k.category_name as cats from shelf k where shelf_nr=1 and
+-- ivm_serial_number='54623' and ivm_manuf ='Mars Inc.';
+--  3o, Ver subcategorias da Prateleira (Aka desdobrar arvore toda)
+--  4o, ver se alguma dessas subcategorias e igual a do produto
+--  PassosB:
+--  1o, Ver Prateleira
+--  2o, Ver categoria da Prateleira
+--	3o, Ver has_category do produto
+--  4o, Ver se categoria da prateleira esta nas do produto
+
+---SELECT category_name
+---FROM shelf
+---WHERE ivm_manuf = NEW.ivm_manuf
+---AND ivm_serial_number = NEW.ivm_serial_number
+---AND shelf_nr = NEW.shelf_nr
+---AND category_name NOT IN (
+---SELECT category_name
+---FROM has_category
+---WHERE product_ean = NEW.product_ean)
+---;
+--
+CREATE OR REPLACE FUNCTION chk_insert_planogram()
+RETURNS TRIGGER AS
+$$
+BEGIN
+	IF NOT EXISTS (
+		SELECT category_name
+		FROM shelf
+		WHERE ivm_manuf = NEW.ivm_manuf
+		AND ivm_serial_number = NEW.ivm_serial_number
+		AND shelf_nr = NEW.shelf_nr
+		AND category_name IN (
+			SELECT category_name
+			FROM has_category
+			WHERE product_ean = NEW.product_ean
+			)
+		)
+	THEN
+		RAISE EXCEPTION 'Products categories not present in selected shelf';
+	END IF;
+	RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER chk_insert_planogram
+BEFORE INSERT ON planogram
+FOR EACH ROW EXECUTE PROCEDURE chk_insert_planogram();
+
+--CREATE OR REPLACE FUNCTION chk_insert_planogram()
+--RETURNS TRIGGER AS
+--$$
+--BEGIN
+--	SELECT p.category_name, s.category_name 
+--	FROM product p, shelf s
+--	WHERE p.product_ean = NEW.product_ean
+--	AND s.category_name= NEW.category_name;
+--	IF p.category_name NOT IN (
+--		SELECT *
+--	)
+--END;
+--$$ LANGUAGE plpgsql;
+--
+--CREATE OR REPLACE FUNCTION chk_insert_planogram()
+--RETURNS TRIGGER AS
+--$$
+--BEGIN
+--	IF 
+--	p.category_name != 
+--	ELSE IF 
+--		p.category_name
+--		FROM (
+--			SELECT product p, shelf s
+--			WHERE p.product_ean = NEW.product_ean
+--			AND s.category_name= NEW.category_name
+--		)
+--		NOT IN (
+--			SELECT * 
+--			FROM has_other h, category s
+--			WHERE h.super_category = s.category_name
+--			OR h.super_category IN (
+--				SELECT child_category
+--				FROM has_other
+--				WHERE super_category = s.category_name
+--			)
+--		)
+--	END IF;
+--END;
+--$$ LANGUAGE plpgsql;
+
+create table retailer (
+	retailer_tin numeric(16,0) not null,
+	retailer_name varchar(255) not null unique,
+	constraint pk_retailer primary key(retailer_tin)
+);
+
+create table responsible_for (
+	ivm_serial_number numeric(5,0) not null,
+	ivm_manuf varchar(255) not null,
+	retailer_tin numeric(16,0) not null,
+	category_name varchar(255) not null,
+	constraint pk_responsible_for primary key(ivm_serial_number, ivm_manuf, retailer_tin, category_name),
+	constraint fk_responsible_for_ivm foreign key(ivm_serial_number, ivm_manuf) references ivm(ivm_serial_number, ivm_manuf),
+	constraint fk_responsible_for_retailer foreign key(retailer_tin) references retailer(retailer_tin),
+	constraint fk_responsible_for_category foreign key(category_name) references category(category_name)
+);
+
+create table replenishment_event (
+	product_ean char(13) not null,
+	shelf_nr numeric(16,0) not null,
+	ivm_serial_number numeric(5,0) not null,
+	ivm_manuf varchar(255) not null,
+	event_instant varchar(255) not null,
+	replenished_units numeric(3, 0) not null,
+	retailer_tin numeric(16,0) not null,
+	constraint pk_replenishment_event primary key(product_ean, shelf_nr, ivm_serial_number, ivm_manuf, event_instant),
+	constraint fk_replenishment_event_planogram foreign key(product_ean, shelf_nr, ivm_serial_number, ivm_manuf) references planogram(product_ean, shelf_nr, ivm_serial_number, ivm_manuf),
+	constraint fk_replenishment_event_retailer foreign key(retailer_tin) references retailer(retailer_tin)
+);
+
+CREATE OR REPLACE FUNCTION chk_units_exceeded()
+RETURNS TRIGGER AS
+$$
+DECLARE
+	T planogram%ROWTYPE;
+BEGIN
+	SELECT *
+	INTO T
+	FROM planogram
+	WHERE product_ean = NEW.product_ean;
+	IF T.units < NEW.replenished_units THEN
+		RAISE EXCEPTION 'Number of replenished units may not exceed the number of allowed units set in the planogram';
+	END IF;
+
+	RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER chk_units_replenishment_event
+BEFORE UPDATE OR INSERT ON replenishment_event 
+FOR EACH ROW EXECUTE PROCEDURE chk_units_exceeded();
