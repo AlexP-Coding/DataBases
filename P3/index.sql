3b715b48009fd62ec9fcede7af3dcf25da85934e
Alexisminas <alexisminas308@gmail.com>
2022-06-24 20:37:33 +0100

    finalized report

--- P3/index.sql
+++ P3/index.sql
@@ -3,7 +3,7 @@ SELECT DISTINCT R.nome
 FROM retalhista R, responsavel_por P 
 WHERE R.tin = P.tin and P. nome_cat = 'Frutos'
 
-/* Assuminndo que as entradas de cada tabela são relativamente
+/* Assumindo que as entradas de cada tabela são relativamente
 estáticas e não há modificações frequentes:*/
 CREATE INDEX retailer_name_index ON retailer USING hash(retailer_name); 
 /*
@@ -17,23 +17,23 @@ CREATE INDEX category_name_index ON responsible_for USING hash(category_name);
  hash(nome_cat) em responsavel_por: 
   Dessa forma, compara-se apenas categorias com o mesmo
  hash code (e, por isso, de menores sub-grupos da tabela) que 'Frutos', 
- diminuindo-se o nr entradas a comparar/percorrer e por isso dimiuindo o tempo de execução 
+ diminuindo-se o número entradas a comparar/percorrer e por isso dimiuindo o tempo de execução 
 */
-CREATE INDEX retailer_tin ON responsible_for USING hash(retailer_tin);
+CREATE INDEX responsible_for_tin_index ON responsible_for USING hash(retailer_tin);
 /*
  hash(tin) em responsavel_por: 
   Dessa forma, compara-se apenas os tin de responsavel_por 
 	com o mesmo hash code (e, por isso, de menores sub-grupos da tabela) que o 
 	dos tin do retalhista, 
-	diminuindo-se o nr de entradas a comparar/percorrer e por isso dimiuindo o tempo de execução 
+	diminuindo-se o número de entradas a comparar/percorrer e por isso dimiuindo o tempo de execução 
 */
-CREATE INDEX retailer_tin ON retailer USING hash(retailer_tin);
+CREATE INDEX retailer_tin_index ON retailer USING hash(retailer_tin);
 /*
  hash(tin) seria preferível ao default btree(tin) em retalhista: 
   Dessa forma, compara-se apenas os tin de responsavel_por 
 	com o mesmo hash code (e, por isso, de menores sub-grupos da tabela) que o 
 	dos tin do retalhista,
-	diminuindo-se o nr de entradas a comparar/percorrer e por isso dimiuindo o tempo de execução 
+	diminuindo-se o número de entradas a comparar/percorrer e por isso dimiuindo o tempo de execução 
 */
 
 
@@ -43,16 +43,19 @@ FROM produto P, tem_categoria T
 WHERE p.cat = T.nome and P.desc like 'A%' 
 GROUP BY T.nome
 
-CREATE INDEX category_name_index ON product USING hash(category_name); 
+CREATE INDEX product_category_name_index ON product USING hash(category_name); 
+CREATE INDEX has_category_name_index ON has_category USING hash(category_name); 
 /*
- hash(cat) em produto: 
-   usando a mesma hash function, facilita-se comparação p.cat = T.nome 
-	e group by pois reduz-se apenas a comparação e a divisão do agrupamento
-	a entradas com um mesmo hash code
+ hash(cat) em produto, hash(nome) em tem_categoria: 
+   usando a mesma hash function, facilita-se comparação "P.cat = T.nome" 
+	e "group by" pois reduz-se apenas a comparação e a divisão dos agrupamentos
+	a entradas com um mesmo hash code (e, por isso, de menores sub-grupos das suas respetivas tabelas),
+	diminuindo-se o número de entradas a comparar/percorrer e por isso dimiuindo o tempo de execução
 */
-CREATE INDEX desc_index ON product USING hash(product_descr);
+CREATE INDEX product_descr_index ON product(product_descr);
 /*
  btree(desc) em produto: 
-   reduz-se procura a entradas no ramo da árvore de palavras iniciadas 
-	pelas primeiras k letras do alfabeto ('A' inclusive)
+   Usando btree, reduz-se procura a entradas no ramo da árvore de palavras iniciadas 
+	por 'A' que, sendo a primeira letra do alfabeto, é um ramo que não demora a ser encontrado;
+	assim, diminui-se o número de entradas a comparar/percorrer e por isso dimiui-se o tempo de execução
 */
